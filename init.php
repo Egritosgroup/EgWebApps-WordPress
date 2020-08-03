80723c21fd141ba6bd87425a84e0aae98654f94a
aliatsos <aliatsos@egritosgroup.gr>
2020-08-03 13:16:40 +0300

    v1.3

--- init.php
+++ init.php
@@ -28,16 +28,8 @@ function initRequires() {
     require_once(EGR__PLUGIN_DIR . 'libs/auth-settings.php');
 
     require_once(EGR__PLUGIN_DIR . 'libs/BFIGitHubPluginUploader.php');
-    if (is_admin()) {
-        if( ! class_exists( 'Smashing_Updater' ) ){
-            include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
-        }
-        
-        $updater = new Smashing_Updater( __FILE__ );
-        $updater->set_username( 'Egritosgroup' );
-        $updater->set_repository( 'EgWebApps-WordPress');
-        //$updater->authorize( '91945bf7a6c75a39f72c7d76f27fe11f33eaaa8b' ); // Your auth code goes here for private repos
-        $updater->initialize();
+    if ( is_admin() ) {
+        new BFIGitHubPluginUpdater( __FILE__, 'Egritosgroup', "EgWebApps-WordPress");
     }
     
 } 
