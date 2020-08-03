fb9fa0fe9f13e9e690975be489779a8600149c97
3 <3423>
2020-08-03 16:42:51 +0300

    version 2 2 8764[0].2 2 8764[1].8805

--- init.php
+++ init.php
@@ -3,13 +3,13 @@
 * Plugin Name: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
 * Plugin URI: 
 * Description: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
-* Version: 2.2.8764
+* Version: 2 2 8764[0].2 2 8764[1].8805
 * Author: EgritosGroup
 * Author URI: https://egritosgroup.gr/
 **/ 
 
 global $egwebapps_version;
-$egwebapps_version = '2.2.8764';
+$egwebapps_version = '2 2 8764[0].2 2 8764[1].8805';
 
 define( 'EGR__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
