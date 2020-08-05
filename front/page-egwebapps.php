e23e75046ffd44e5b688a8e8ceb59010a3903ae8
aliatsos <aliatsos@egritosgroup.gr>
2020-08-05 19:32:46 +0300

    commit

---
+++ front/page-egwebapps.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+<?php include dirname( __FILE__ ) . '/view-egwebapps.php'; ?>
+<?php get_footer(); ?>
--- front/view-egwebapps.php
+++ front/view-egwebapps.php
@@ -20,7 +20,7 @@ $options = get_option('egr_webapps_plugin_options');
     var egwebappsVersion = '<?php echo $egwebapps_version; ?>';
 </script>
 <?php if(isset($options['jquery_check']) && $options['jquery_check'] == 'on') { ?>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" crossorigin="anonymous"></script>
 <?php } ?>
 <script src="<?php echo site_url('/' . trailingslashit(dirname(plugin_basename( __FILE__ ))) . 'auth-settings'); ?>?v=<?php echo $egwebapps_version; ?>"></script>
 
