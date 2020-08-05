79f1dc0aa296b9846574ac87178a5eedfeda9da6
aliatsos <aliatsos@egritosgroup.gr>
2020-08-05 18:40:20 +0300

    ccc

--- front/view-egwebapps.php
+++ front/view-egwebapps.php
@@ -2,29 +2,27 @@
 $options = get_option('egr_webapps_plugin_options');
 get_header(); ?>
 
+<link href="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/'); ?>styles.css?v=<?php echo $egwebapps_version; ?>" rel='stylesheet' type='text/css'>
 <?php if(!empty($options['extra_css_path'])) { ?>
     <link href="<?php echo site_url() . $options['extra_css_path']; ?>?v=<?php echo $GLOBALS["egritos_version"]; ?>" rel='stylesheet' type='text/css'>
 <?php } ?>
-
-<?php if(isset($options['jquery_check']) && $options['jquery_check'] == 'on') { ?>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<?php if(!empty($options['extra_css'])) { ?>
+    <style>
+        <?php echo $options['extra_css']; ?>
+    </style>
 <?php } ?>
 
 <script>
     var baseHeaderTag = document.createElement('base');
     baseHeaderTag.href ="/egwebapps/";
     document.getElementsByTagName('head')[0].prepend(baseHeaderTag);
+
+    var egwebappsVersion = '<?php echo $egwebapps_version; ?>';
 </script>
-
-<?php if(!empty($options['extra_css'])) { ?>
-    <style>
-        <?php echo $options['extra_css']; ?>
-    </style>
+<?php if(isset($options['jquery_check']) && $options['jquery_check'] == 'on') { ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <?php } ?>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script> -->
-
 <script src="<?php echo site_url('/' . trailingslashit(dirname(plugin_basename( __FILE__ ))) . 'auth-settings'); ?>?v=<?php echo $egwebapps_version; ?>"></script>
-<link href="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/front/css/'); ?>egwebapps-base.css?v=<?php echo $GLOBALS["egritos_version"]; ?>" rel='stylesheet' type='text/css'>
 
 <script src="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/'); ?>common-es5.js?v=<?php echo $egwebapps_version; ?>" nomodule></script>
 <script src="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/'); ?>runtime-es5.js?v=<?php echo $egwebapps_version; ?>" nomodule></script>
@@ -47,8 +45,6 @@ get_header(); ?>
 
 <app-root></app-root>
 
-<div class="plugin-footer">
-v <?php echo $egwebapps_version; ?>
-</div>
+
 
 <?php get_footer(); ?>
