e092c6b31c47f56352c9f34345719bae667d34da
3 <3423>
2021-03-23 09:27:04 +0200

    version 10.1.12633

--- init.php
+++ init.php
@@ -3,13 +3,13 @@
 * Plugin Name: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
 * Plugin URI: 
 * Description: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
-* Version: 2.2.12605
+* Version: 10.1.12633
 * Author: EgritosGroup
 * Author URI: https://egritosgroup.gr/
 **/ 
 
 global $egwebapps_version;
-$egwebapps_version = '2.2.12605';
+$egwebapps_version = '10.1.12633';
 
 
 $GLOBALS['pluginFolderName'] = explode('/', trailingslashit(dirname(plugin_basename( __FILE__ ))));
--- static/assets/auth-config.js
+++ static/assets/auth-config.js
@@ -1,10 +1,12 @@
 var AuthConfOptions = {
 	stsServer: "http://auth.dev.egritosgroup.gr",
+	//stsServer: "https://auth.demo.egritosgroup.gr",
 	redirect_url: "http://localhost:4200/redirect-auth",
 	app_base_href: "/",
 	// The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
 	// The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
 	client_id: "egwebapps-localhost",
+	//client_id: "egwebapps-egritos",
 	response_type: "id_token token",
 	// For some oidc, we require resource identifier to be provided along with the request.
 	resource: "",
@@ -37,10 +39,11 @@ var AppConfOptions = {
 	unauthorized_allowed: true,
 	oroi_xrisis_link: "",
 	home_page_text: '<p>Σχόλια που αφορούν τις βεβαιωμένες οφειλές.</p><p>Σχόλια που αφορούν τις αποδείξεις των βεβαιωμένων οφειλών.</p>',
+	order_profile: 200,
 
 	is_benefits_enabled: true,
 	benefits_applications_organization: "ΔΟΜΕΣ",
-	benefits_api: "http://paidi.demo.egritosgroup.gr/",
+	benefits_api: "https://domes.demo.egritosgroup.gr/",
 	benefits_tab_title: "Αιτήσεις Παιδικών Σταθμών",
 	benefits_description: 'Δημιουργία, επεξεργασία & υποβολή αιτήσεων για παιδικούς σταθμούς, με άμεση ενημέρωση για την κατάσταση της αίτησής σας.',
 	benefits_tab_info: 'Δημιουργία, επεξεργασία & υποβολή αιτήσεων για παιδικούς σταθμούς, με άμεση ενημέρωση για την κατάσταση της αίτησής σας.',
@@ -51,6 +54,7 @@ var AppConfOptions = {
 	carpooling_description: 'Δημιουργία & συμμετοχή σε διαδρομές συν-πολιτών σας από οποιοδήποτε μέρος της Ελλάδας.',
 	carpooling_tab_info: '<p>Δημιουργία & συμμετοχή σε διαδρομές συν-πολιτών σας από οποιοδήποτε μέρος της Ελλάδας.</p>',
 	google_maps_key: "AIzaSyCIE9XxZEUpWvs_4e1O-gKL1jK6Xo2CQCM",
+	order_carpooling: 170,
 
 	is_epayments_enabled: true,
 	epayments_api: "http://financeapi.dev.egritosgroup.gr/",
@@ -59,6 +63,7 @@ var AppConfOptions = {
 	epayments_debits_tab_info: 'Σχόλια που αφορούν τις βεβαιωμένες οφειλές.',
 	epayments_receipts_tab_info: 'Σχόλια που αφορούν τις αποδείξεις των βεβαιωμένων οφειλών.',
 	show_non_persisted_debits: false,
+	order_epayments: 10,
 
 	is_kliseis_enabled: true,
 	kliseis_api: "http://kliseis.demo.egritosgroup.gr/",
@@ -68,39 +73,47 @@ var AppConfOptions = {
 				"Καταχώρηση Νέας Κλήσης".`,
 	kliseis_receipts_tab_info: 'Ιστορικό πληρωμών κλήσεων.',
 	kliseis_unauthorized_info: 'Δεν έχετε δικαίωμα πρόσβασης.',
+	order_kliseis: 20,
 
 	is_eidopoiitiria_enabled: true,
 	eidopoiitiria_api: "http://financeapi.dev.egritosgroup.gr/",
+	//eidopoiitiria_api: "https://financeapi.localhost.egritosgroup.gr/",
 	eidopoiitiria_description: 'Εξόφληση ταυτοτήτων οφειλών.',
-	eidopoiitiria_tab_info: '',
+	eidopoiitiria_tab_info: 'Εξόφληση ταυτοτήτων οφειλών.',
+	order_eidopoiitiria: 70,
 
 	is_dimotikos_foros_enabled: true,
-	katastimata_api: "http://katastimata.demo.egritosgroup.gr/",
+	katastimata_api: "http://katastimata.dev.egritosgroup.gr/",
 	katastimata_description: 'Υποβάλλετε και εξοφλήστε την δήλωσή σας για το τέλος 0,5%.',
 	katastimata_tab_info: 'Υποβάλλετε και εξοφλήστε την δήλωσή σας για το τέλος 0,5%.',
+	order_katastimata: 30,
 
 	is_diakanonismoi_enabled: true,
 	diakanonismoi_description: 'Ενημέρωση για την κατάσταση των διακανονισμών σας & αίτηση νέων διακακονισμών.',
 	diakanonismoi_tab_info: 'Ενημέρωση για την κατάσταση των διακανονισμών σας & αίτηση νέων διακακονισμών.',
+	order_diakanonismoi: 40,
 
-	is_shde_app_enabled: true,
+	is_shde_app_enabled: false,
 	shde_api: "http://prot.dev.egritosgroup.gr/",
 	shde_description: 'Αιτήσεις Πολιτών προς το σύστημα ηλεκτρονικής διακίνησης εγγράφων του οργανισμού.',
 	shde_tab_info: 'Αιτήσεις Πολιτών προς το σύστημα ηλεκτρονικής διακίνησης εγγράφων του οργανισμού.',
+	order_shde: 90,
 
-	is_rantevou_app_enabled: true,
+	is_rantevou_app_enabled: false,
 	rantevou_api: "http://rantevou.demo.egritosgroup.gr/",
 	rantevou_description: '',
-	rantevou_tab_info: '',
+	rantevou_tab_info: 'Ραντεβού Πολιτών',
+	order_rantevou: 100,
 
-	//PROSLIPSEIS
-	is_proslipseis_enabled: true,
-	proslipseis_api: "http://proslipseis.demo.egritosgroup.gr/",
+	is_proslipseis_enabled: false,
+	proslipseis_api: "https://proslipseis.demo.egritosgroup.gr/",
 	proslipseis_description: 'Αιτήσεις Πολιτών προς το σύστημα προσλήψεων εποχιακού και μόνιμου προσωπικού του οργανισμού.',
 	proslipseis_tab_title: "Αιτήσεις Προσλήψεων",
 	proslipseis_tab_info: 'Αιτήσεις Πολιτών προς το σύστημα προσλήψεων εποχιακού και μόνιμου προσωπικού του οργανισμού.',
+	order_proslipseis: 60,
 
 	is_notifications_enabled: false,
-	notifications_api: "https://localhost:5330/"
+	notifications_api: "https://localhost:5330/",
+	order_notifications: 80
 };
 
