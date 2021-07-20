12a0d6f07607b22bebf0cf750023df10ddda1f09
aliatsos <aliatsos@egritosgroup.gr>
2021-07-20 12:00:09 +0300

    new options

--- front/page-egwebapps.php
+++ front/page-egwebapps.php
@@ -1,6 +1,3 @@
 <?php get_header(); ?>
         <?php require_once("view-egwebapps.php"); ?>
         <?php get_footer(); ?>
-<?php get_header(); ?>
-        <?php require_once("view-egwebapps.php"); ?>
-        <?php get_footer(); ?>
