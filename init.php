e03f2dfcafae44c9821302a3833212208cd5c4bc
Tfs Administrator <Administrator@egritosgroup.gr>
2022-09-09 12:03:34 +0300

    version 13.0.23304

--- init.php
+++ init.php
@@ -3,13 +3,13 @@
 * Plugin Name: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
 * Plugin URI: 
 * Description: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
-* Version: 13.0.23274
+* Version: 13.0.23304
 * Author: EgritosGroup
 * Author URI: https://egritosgroup.gr/
 **/ 
 
 global $egwebapps_version;
-$egwebapps_version = '13.0.23274';
+$egwebapps_version = '13.0.23304';
 
 
 $GLOBALS['pluginFolderName'] = explode('/', trailingslashit(dirname(plugin_basename( __FILE__ ))));
