c394683b71b223289d71c5c07583ca0fc7510cc5
Ria Stamopoulou <rstamopoulou@egritosgroup.gr>
2022-09-09 12:01:20 +0300

    fix: angular 13 script files

--- front/front-init.php
+++ front/front-init.php
@@ -1,52 +1,60 @@
 <?php //show plugin template if theme template not found
-$options = get_option( 'egr_webapps_plugin_options' );
+$options = get_option('egr_webapps_plugin_options');
 
-if(isset($options['shortcode']) && $options['shortcode'] == 'on') {
-    add_filter('request', function ( $query_vars ) {
-		
-        if(strpos($_SERVER['REQUEST_URI'],'/egwebapps') > -1) {
-			$query_vars['pagename'] = 'egwebapps';
-        }
-        
-        return $query_vars;
-    } );
+if (isset($options['shortcode']) && $options['shortcode'] == 'on') {
+   add_filter('request', function ($query_vars) {
 
-    function wpb_shortcode_egwebapps() { 
-        ob_start();
-        include dirname( __FILE__ ) . '/view-egwebapps.php';
-        return ob_get_clean();
+      if (strpos($_SERVER['REQUEST_URI'], '/egwebapps') > -1) {
+         $query_vars['pagename'] = 'egwebapps';
+      }
 
-    }
-    add_shortcode('shortcode-egwebapps', 'wpb_shortcode_egwebapps');
+      return $query_vars;
+   });
+
+   function wpb_shortcode_egwebapps()
+   {
+      ob_start();
+      include dirname(__FILE__) . '/view-egwebapps.php';
+      return ob_get_clean();
+   }
+   add_shortcode('shortcode-egwebapps', 'wpb_shortcode_egwebapps');
 } else {
-    add_action('parse_request', 'egwebapps_url_handler');
-    function egwebapps_url_handler() {
-        if(strpos($_SERVER['REQUEST_URI'],'/egwebapps') > -1) {
-            if(file_exists(get_template_directory().'/page-egwebapps.php')) {
-                require_once(get_template_directory().'/page-egwebapps.php');
-            } else {
-                require_once('page-egwebapps.php');
-            }
-            exit();
-        }
-    }
+   add_action('parse_request', 'egwebapps_url_handler');
+   function egwebapps_url_handler()
+   {
+      if (strpos($_SERVER['REQUEST_URI'], '/egwebapps') > -1) {
+         if (file_exists(get_template_directory() . '/page-egwebapps.php')) {
+            require_once(get_template_directory() . '/page-egwebapps.php');
+         } else {
+            require_once('page-egwebapps.php');
+         }
+         exit();
+      }
+   }
 }
 
-function sortFilesfromFolder($a, $b) {
-    $a = explode('-', $a);
-    $b = explode('-', $b);
-    
-    if ($a[0] == $b[0]) {
-        return 0;
-    }
-    return ($a[0] < $b[0]) ? -1 : 1;
+function sortFilesfromFolder($a, $b)
+{
+   $a = explode('-', $a);
+   $b = explode('-', $b);
+
+   if ($a[0] == $b[0]) {
+      return 0;
+   }
+   return ($a[0] < $b[0]) ? -1 : 1;
 }
 
-function IncludeAngularFiles() {
-    $egwebapps_version = $GLOBALS["egwebapps_version"];
+function IncludeAngularFiles()
+{
+   $egwebapps_version = $GLOBALS["egwebapps_version"];
 
-    if($handle = opendir('./wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/')) {
-        $es5Files = [];
+   if ($handle = opendir('./wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/')) { ?>
+      <script src="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/'); ?>runtime.js?v=<?php echo $egwebapps_version; ?>" type="module"></script>
+      <script src="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/'); ?>polyfills.js?v=<?php echo $egwebapps_version; ?>" type="module"></script>
+      <script src="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/'); ?>scripts.js?v=<?php echo $egwebapps_version; ?>" type="module"></script>
+      <script src="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/'); ?>main.js?v=<?php echo $egwebapps_version; ?>" type="module"></script>
+
+<?php /* $es5Files = [];
         $es2015Files = [];
         $es5mainFiles = [];
         $es2015mainFiles = [];
@@ -96,8 +104,8 @@ function IncludeAngularFiles() {
             <script src="<?= site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/') . $i; ?>?v=<?= $egwebapps_version; ?>" type="module"></script>
         <?php } ?>
         
-        <script src="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/'); ?>scripts.js?v=<?php echo $egwebapps_version; ?>" defer></script>
+        <script src="<?php echo site_url('/wp-content/plugins/' . $GLOBALS['pluginFolderName'][0] . '/static/'); ?>scripts.js?v=<?php echo $egwebapps_version; ?>" defer></script> */
 
-        <?php closedir($handle);
-    }  
+      closedir($handle);
+   }
 } ?>
