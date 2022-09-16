69d50b2340fd38dd76b1a11a776745309f715172
Tfs Administrator <Administrator@egritosgroup.gr>
2022-09-16 15:19:36 +0300

    version 13.0.23453

--- static/assets/auth-config.js
+++ static/assets/auth-config.js
@@ -35,9 +35,9 @@ var AppConfOptions = {
 
 	is_benefits_enabled: true,
 	benefits_applications_organization: "ΔΟΜΕΣ",
-	benefits_api: "https://domes.demo.egritosgroup.gr/",
+	//benefits_api: "https://domes.demo.egritosgroup.gr/",
 	benefits_afm_required: false,
-	//benefits_api: "http://localhost:5380/",
+	benefits_api: "http://localhost:5380/",
 	benefits_tab_title: "Αιτήσεις Παιδικών Σταθμών",
 	benefits_tab_title_en: "Nursery Schools Applications",
 	benefits_description: 'Δημιουργία, επεξεργασία και υποβολή αιτήσεων για παιδικούς σταθμούς, με άμεση ενημέρωση για την κατάσταση της αίτησής σας.',
