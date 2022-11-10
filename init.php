9b591d26f120ef6aab761757b47569dd833ce56a
3 <3423>
2022-11-10 14:39:57 +0200

    version 13.0.24956

--- init.php
+++ init.php
@@ -3,13 +3,13 @@
 * Plugin Name: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
 * Plugin URI: 
 * Description: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
-* Version: 13.0.24820
+* Version: 13.0.24956
 * Author: EgritosGroup
 * Author URI: https://egritosgroup.gr/
 **/ 
 
 global $egwebapps_version;
-$egwebapps_version = '13.0.24820';
+$egwebapps_version = '13.0.24956';
 
 
 $GLOBALS['pluginFolderName'] = explode('/', trailingslashit(dirname(plugin_basename( __FILE__ ))));
