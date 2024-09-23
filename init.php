7406b5a46b0848481420ecf0d6b0d2e8867ab07c
3 <3423>
2024-09-23 10:56:12 +0300

    version 17.0.37651

--- init.php
+++ init.php
@@ -3,13 +3,13 @@
 * Plugin Name: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
 * Plugin URI: 
 * Description: Ηλεκτρονικές Εφαρμογές - Έγκριτος Group
-* Version: 17.0.37567
+* Version: 17.0.37651
 * Author: EgritosGroup
 * Author URI: https://egritosgroup.gr/
 **/ 
 
 global $egwebapps_version;
-$egwebapps_version = '17.0.37567';
+$egwebapps_version = '17.0.37651';
 
 
 $GLOBALS['pluginFolderName'] = explode('/', trailingslashit(dirname(plugin_basename( __FILE__ ))));
